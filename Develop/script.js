52eb9bdd57b8f54e8e1f5749a369dcf3e45b7d1d
britbolt <blattisaw@gmail.com>
2022-05-20 20:06:43 -0400

    updated random string logic and readme

--- Develop/script.js
+++ Develop/script.js
@@ -1,73 +1,72 @@
 // Define var myPassword and characters 
-var myPassword = ' ';
-  const uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  const lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz';
-  const numbers = 1234567890;
-  const specials = '!@#$%^&*()_;<>?":;=+{}[]/~`-.,';
-  let password = uppercaseLetters.concat(lowercaseLetters + numbers + specials);
+const uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+const numbers = 1234567890;
+const specials = '!@#$%^&*()_;<>?":;=+{}[]/~`-.,';
+
+
 
   //password length prompt
- var passwordLength = window.prompt("How long should your password be? Enter a number between 8 and 25");
-if (passwordLength >8, passwordLength <25) {
-  let length = password.length;
-}
-else { 
-  window.alert("Please select a number between 8 and 25.");
-}
-console.log(passwordLength);
+ var lenPass = window.prompt("How long should your password be? Enter a number between 8 and 25");
+ var lowercase = window.confirm("Would you like lowercase letters?");
+ var uppercase = window.confirm("Would you like uppercase letters?");
+ var number = window.confirm("Would you like numbers in your password?");  
+ var symbols = window.confirm("Would you like special characters in your password?");
 
-/*
-//uppercase confirmation
-  var uppercase = window.confirm("Would you like uppercase letters in your password? If yes, please select okay.");
-  if (uppercase) {
-    let password = uppercaseLetters.concat(numbers + specials);
-  }
-  else {
-    let password = uppercaseLetters;
-  }
+ console.log(lenPass);
+  console.log(lowercase, uppercase, number, symbols);
+
+  const newArray = [];
+if (lenPass > 8 && lenPass < 25) {
   
-  var number = window.confirm("Would you like numbers in your password? If yes, please select okay.")
-  if (number) {
-    let password = number + uppercase;
-  }
-  else {
-    let password = uppercase;
-  }
+    if (lowercase) {
+    newArray.push(lowercaseLetters)};
 
-  var specialCharacters = window.confirm("Would you like special characters in your password? If yes, please select okay.")
-  if (specialCharacters) {
-    let password = specialCharacters + number + uppercase;
-  }
-else {
-  let password = number + uppercase;
-}*/
+     if(number) {
+        newArray.push(numbers)};
+
+      if(symbols) {
+          newArray.push(specials)};
+
+      if(uppercase) {
+        newArray.push(uppercaseLetters)};
+
+        
+    console.log(newArray);
+  };
 
 
+//let password = '';
+function createPassword() {
+  let password = newArray.join("");
+  console.log(password);
 
-  function generatePassword(length) {
-   for (let i = 0; i < length; i++) {
-     var passwordLength = password.length;
-    myPassword += password[Math.floor(Math.random() * passwordLength)];
+  let lenWord = parseInt(lenPass);
+  console.log(lenWord);
+ 
+  let newPassword = [];
+
+     for (var i = 0; i < lenWord; i++) {
+         var myPassword = password[Math.floor(Math.random() * password.length)];
+         newPassword.push(myPassword);
+         console.log(myPassword);
+         //myPassword += password.substring(rpass, rpass +1);
    }
-  return myPassword;
-}
+   let outputPass = newPassword.join('');
+   var passwordText = document.querySelector("#password");
+    passwordText.value = outputPass;
+  };
 
-//if (passwordLength === 25<i>8  || passwordLength === "password.length") 
+  
+ 
 
-
-// Get references to the #generate element
+// // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", createPassword);
+
+
+
 
 
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Password Generator Starter Code
+# Password Generator 
+
+## Description
+This application allows the user to generate a random unique password that contains between 8 and 25 characters. The user has the oppportunity to select their parameters such as lowercase, uppercase, numbers and special characters. Once the user goes through the prompts to select their preferences the generate button will display a unique password. If the user presses the generate button again they will be shown another unique password of the same length and with the same parameters they previously selected.
+
+## Technologies used
+- html
+- css
+- Javascript
+
+## Usage
+In order to use the application go to the live deployed link for the application and respond to the prompts.
+
+![screenshot showing example of password generation](/Develop/assets/images/screenshot-sample-password.jpg)
+
+## Contributors
+Brittany B
+
+## Questions
+Contact me on githun
